85de8a680fa61cd1dc18fb0ed17f128dd57a8844
Andy Au <andyau.aa@gmail.com>
2019-08-23 12:10:24 -0700

    refactored linkedSubDetailLayout so that index could use it

--- components/layouts/LinkedSubDetailLayout.js
+++ components/layouts/LinkedSubDetailLayout.js
@@ -6,16 +6,27 @@ class LinkedSubDetailLayout extends React.Component {
   // Ensure data is valid and if so, create a list element containing a URL to the detail page of each array element.
   renderSubDetail(data, endpoint) {
     if (data && data[0] !== null) {
-      return data.map((url) => {
-        // Get ID of each sub-detail by extracting the number from it's URL.
-        const id = url.match(/[0-9]+/g);
+      // Get ID of each sub-detail by extracting the number from it's URL.
+      return data.map((item) => {
+        let id = null;
+        let text = null;
+        // Check to see if url is nested inside of another variable.
+        // Also determine the text to display.
+        if (item.url) {
+          id = item.url.match(/[0-9]+/g);
+          text = item.title;
+        } else {
+          id = item.match(/[0-9]+/g)
+          text = `ID ${id}`;
+        }
         return (
           <li key={id}>
             <Link route={`/${endpoint}/${id}`}>
-              <a> ID {id} </a>
+              <a> {text} </a>
             </Link>
           </li>
         )
+
       })
     }
     return null;
@@ -24,20 +35,21 @@ class LinkedSubDetailLayout extends React.Component {
   // Render the data as a list. If there is no data, render `n/a`.
   render() {
     const { alias, endpoint, data } = this.props;
+    let element = null;
     if (data.length == 0 || data[0] === null) {
-      return (
-        <div className="sub-detail">
-          <h4>{alias}</h4>
-          <p>n/a</p>
-        </div>
+      element = <p>n/a</p>;
+    }
+    else {
+      element = (
+        <ul>
+          {this.renderSubDetail(data, endpoint)}
+        </ul>
       )
     }
     return (
       <div className="sub-detail">
         <h4>{alias}</h4>
-        <ul>
-          {this.renderSubDetail(data, endpoint)}
-        </ul>
+        {element}
       </div>
     )
   }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
+import LinkedSubDetailLayout from '../components/layouts/LinkedSubDetailLayout';
 import HttpErrorLayout from '../components/layouts/HttpErrorLayout';
 import BasePage from '../components/BasePage';
-import { Link } from '../routes';
 import PropTypes from 'prop-types';
 import getDataFromSWAPI from '../utils/getReqHelper';
-
-// import axios from 'axios';
 
 class Index extends React.Component {
 
@@ -16,21 +14,6 @@ class Index extends React.Component {
     return { data: data.results }
   }
 
-  // Create HTML element for each array element.
-  renderFilms(data) {
-    return data.map((film) => {
-      // Get the films ID by extracting the number from it's URL.
-      const id = film.url.match(/[0-9]+/g)
-      return (
-        <li key={id}>
-          <Link route={`/film/${id}`}>
-            <a style={{ 'fontSize': '20px' }}> {film.title} </a>
-          </Link>
-        </li>
-      )
-    })
-  }
-
   // Render data if valid or error otherwise.
   render() {
     const { data } = this.props;
@@ -38,10 +21,7 @@ class Index extends React.Component {
       return (
         <BaseLayout className="cover">
           <BasePage>
-            <h1>Film List</h1>
-            <ul>
-              {this.renderFilms(data)}
-            </ul>
+            <LinkedSubDetailLayout alias="Film List" endpoint="film" data={data} />
           </BasePage>
         </BaseLayout>
       )
